4cf168edda31f33db411d5ae914b46c95d02d01c
JoseViera <josvile@gmail.com>
2017-05-05 01:02:48 +0200

    UPLOAD

--- Angular_2/src/app/app.module.ts
+++ Angular_2/src/app/app.module.ts
@@ -3,22 +3,39 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//ROUTES
+import { APP_ROUTING } from './app.routes';
+
+//SERVICES
+import { HeroesService } from './services/heroes.services';
+
+
+//COMPONENTS
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Angular_2/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {HeroesComponent} from './components/heroes/heroes.component';
+import {AboutComponent} from './components/about/about.component';
+import {HeroComponent} from './components/hero/hero.component';
+
+export const ROUTES: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'hero/:id', component: HeroComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'},
+];
+
+export const APP_ROUTING = RouterModule.forRoot(ROUTES, { useHash: true});
